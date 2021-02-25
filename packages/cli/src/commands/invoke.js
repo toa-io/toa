aa712435afbcc4e700f65b427a32a9f4c6d3498f
temich <tema.gurtovoy@gmail.com>
2021-02-25 14:53:07 +0300

    refactor(CLI): Add usage example for invoke command

--- packages/cli/src/commands/invoke.js
+++ packages/cli/src/commands/invoke.js
@@ -2,9 +2,15 @@
 
 const { handler } = require('../handlers/invoke')
 
+const builder = (yargs) => {
+  yargs
+    .usage('Usage: kookaburra invoke sum --input.a=1 --input.b=2')
+    .option('input', {
+      describe: 'Input object'
+    })
+}
+
 exports.command = 'invoke <operation>'
 exports.desc = 'Invoke operation'
-exports.builder = {
-  input: {}
-}
+exports.builder = builder
 exports.handler = handler
