7fc1cb2e039b47eee50570b33702c400c9417399
temich <tema.gurtovoy@gmail.com>
2021-02-28 10:27:05 +0300

    feat(gears): Add yaml.try

--- packages/gears/src/yaml.js
+++ packages/gears/src/yaml.js
@@ -8,8 +8,17 @@ const yaml = require('js-yaml')
 const load = async path => {
   const readFile = util.promisify(fs.readFile)
   const contents = await readFile(path, 'utf8')
+  const object = yaml.load(contents)
 
-  return yaml.load(contents)
+  return object
+}
+
+load.try = async path => {
+  try {
+    return await load(path)
+  } catch (e) {
+    return null
+  }
 }
 
 exports.yaml = load
---
+++ packages/gears/test/yaml.test.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+const { yaml } = require('../src/yaml')
+
+describe('load', () => {
+  it('should return object', async () => {
+    const object = await yaml(path.resolve(__dirname, './yaml.yaml'))
+
+    expect(object?.ok).toBeTruthy()
+  })
+
+  it('should throw on file read error', async () => {
+    const attempt = async () => await yaml(path.resolve(__dirname, './no-file.yaml'))
+
+    await expect(attempt).rejects.toThrow(/ENOENT/)
+  })
+})
+
+describe('try', () => {
+  it('should return object', async () => {
+    const object = await yaml.try(path.resolve(__dirname, './yaml.yaml'))
+
+    expect(object?.ok).toBeTruthy()
+  })
+
+  it('should return null on file read error', async () => {
+    const object = await yaml.try(path.resolve(__dirname, './no-file.yaml'))
+
+    expect(object).toBeNull()
+  })
+})
