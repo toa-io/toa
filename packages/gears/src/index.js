48d025ded0808c56ef6679d235eaa6412077d617
temich <tema.gurtovoy@gmail.com>
2021-03-03 18:57:17 +0300

    feat(gears): Add concat

--- packages/gears/src/index.js
+++ packages/gears/src/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const { concat } = require('./concat')
 const { console } = require('./console')
 const { yaml } = require('./yaml')
 
+exports.concat = concat
 exports.console = console
 exports.yaml = yaml
