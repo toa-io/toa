81fcc5396f40f7b59e27bee8d4182810e28a2e07
temich <tema.gurtovoy@gmail.com>
2022-01-21 02:05:20 +0300

    feat(cli): add deployment export target path argument

--- runtime/cli/src/handlers/deploy/export.js
+++ runtime/cli/src/handlers/deploy/export.js
@@ -8,7 +8,7 @@ const { context: find } = require('../../util/find')
 const dump = async (argv) => {
   const context = find(argv.path)
   const deployment = await boot.deployment(context)
-  const path = await deployment.export()
+  const path = await deployment.export(argv.target)
 
   console.log(path)
 }
