3f5006cc5481000831902e9050ae5d08c07d7ae0
temich <tema.gurtovoy@gmail.com>
2022-07-09 18:10:10 +0300

    refactor(amqp): add usage of connectors.deployment

--- connectors/bindings.amqp/src/deployment.js
+++ connectors/bindings.amqp/src/deployment.js
@@ -1,19 +1,23 @@
 'use strict'
 
 const { Locator } = require('@toa.io/core')
-const { uris } = require('@toa.io/libraries/connectors')
+const connectors = require('@toa.io/libraries/connectors')
 
 const { PREFIX, SYSTEM } = require('./constants')
+
+const { resolve } = connectors.uris
 
 /**
  * @type {toa.operations.deployment.dependency.Constructor}
  */
-const deployment = (instances, annotations) => {
-  /** @type {toa.operations.deployment.Dependency} */
-  const deployment = {}
-  const sys = system(annotations)
+const deployment = (instances, annotation) => {
+  if (annotation === undefined) {
+    throw new Error('AMQP deployment requires either \'system\' or \'default\' URI annotation')
+  }
 
-  deployment.proxies = proxies(instances, annotations)
+  const deployment = connectors.deployment(instances, annotation, PREFIX)
+  const sys = system(annotation)
+
   deployment.proxies.push(sys)
 
   return deployment
@@ -24,31 +28,14 @@ const deployment = (instances, annotations) => {
  * @returns {toa.operations.deployment.dependency.Proxy}
  */
 function system (annotation) {
-  if (annotation === undefined) throw new Error('AMQP deployment requires either \'system\' or \'default\' URI annotation')
-
   const locator = new Locator(SYSTEM)
   const name = locator.hostname(PREFIX)
-  const url = uris.resolve(annotation, locator)
+  const url = resolve(annotation, locator)
   const target = url.hostname
 
   if (target === undefined) throw new Error('AMQP \'system\' URI annotation is mandatory')
 
   return { name, target }
-}
-
-/**
- * @param {toa.norm.context.dependencies.Instance[]} instances
- * @param {toa.connectors.URIs} annotation
- * @returns {toa.operations.deployment.dependency.Proxy[]}
- */
-const proxies = (instances, annotation) => {
-  return instances.map((instance) => {
-    const url = uris.resolve(annotation, instance.locator)
-    const target = url.hostname
-    const name = instance.locator.hostname(PREFIX)
-
-    return { name, target }
-  })
 }
 
 exports.deployment = deployment
--- connectors/bindings.amqp/test/deployment.test.js
+++ connectors/bindings.amqp/test/deployment.test.js
@@ -6,6 +6,8 @@ const mock = require('@toa.io/libraries/mock')
 
 const { deployment } = require('../')
 
+const PREFIX = 'bindings-amqp'
+
 /** @type {toa.norm.context.dependencies.Instance[]} */
 let instances
 
@@ -20,60 +22,29 @@ it('should exist', () => {
   expect(deployment).toBeDefined()
 })
 
-it('should create default proxies', () => {
-  const url = gen()
-  const annotation = { default: url.href }
-
-  /** @returns {toa.operations.deployment.dependency.Proxy[]} */
-  const proxies = instances.map((instance) => ({
-    name: instance.locator.hostname(PREFIX), target: url.hostname
-  }))
-
-  const output = deployment(instances, annotation)
-
-  expect(output.proxies).toStrictEqual(expect.arrayContaining(proxies))
-})
-
-it('should create proxies with given values', () => {
-  const url = gen()
-  const annotation = { system: url.href }
-  const proxies = []
-
-  for (const instance of instances) {
-    const url = gen()
-    const target = url.hostname
-
-    annotation[instance.locator.id] = url.href
-    proxies.push({ name: instance.locator.hostname(PREFIX), target })
-  }
-
-  const output = deployment(instances, annotation)
-
-  expect(output.proxies).toStrictEqual(expect.arrayContaining(proxies))
-})
-
-it('should create system proxy', () => {
-  const url = gen()
-  const annotation = { default: url.href }
-  const instances = []
-
-  const output = deployment(instances, annotation)
-
-  expect(output.proxies).toStrictEqual([{
-    name: PREFIX + '-system',
-    target: url.hostname
-  }])
-})
-
-it('should throw if system is not defined', () => {
-  const annotation = {}
-  const instances = []
-
-  expect(() => deployment(instances, annotation)).toThrow('not found')
-})
-
 it('should throw if annotation is not defined', () => {
-  expect(() => deployment(instances, undefined)).toThrow('AMQP deployment requires either \'system\' or \'default\' URI annotation')
+  expect(() => deployment(instances, undefined))
+    .toThrow('AMQP deployment requires either \'system\' or \'default\' URI annotation')
 })
 
-const PREFIX = 'bindings-amqp'
+describe('proxies', () => {
+  it('should create system proxy', () => {
+    const url = gen()
+    const annotation = { default: url.href }
+    const instances = []
+
+    const output = deployment(instances, annotation)
+
+    expect(output.proxies).toStrictEqual([{
+      name: PREFIX + '-system',
+      target: url.hostname
+    }])
+  })
+
+  it('should throw if system is not defined', () => {
+    const annotation = {}
+    const instances = []
+
+    expect(() => deployment(instances, annotation)).toThrow('not found')
+  })
+})
