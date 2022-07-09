f6f6d6b8b790e6b875d09b240dde1efbcaf82924
temich <tema.gurtovoy@gmail.com>
2022-07-09 18:08:53 +0300

    docs(deployment): export dependency types

--- operations/types/deployment/dependency.d.ts
+++ operations/types/deployment/dependency.d.ts
@@ -5,67 +5,67 @@ import { dependencies } from '@toa.io/norm/types/context'
 
 declare namespace toa.operations.deployment {
 
-    namespace dependency {
+  namespace dependency {
 
-        type Constructor = (declarations: dependencies.Instance[], annotations: any) => Dependency
+    type Constructor = (declarations: dependencies.Instance[], annotation: any) => Dependency
 
-        interface Reference {
-            name: string
-            version: string
-            repository?: string
-            alias?: string
-            values?: Object
-        }
-
-        interface Service {
-            group: string
-            name: string
-            version: string
-            port: number
-            ingress?: {
-                host: string
-                class: string
-                annotations?: object
-            }
-        }
-
-        interface Proxy {
-            name: string
-            target: string
-        }
-
-        interface Variable {
-            name: string
-            value: string
-        }
-
-        interface Variables {
-            [key: string]: Variable[]
-        }
-
-        interface Declaration {
-            references?: Reference[]
-            services?: Service[] // dependency.Service
-            proxies?: Proxy[]
-            variables?: Variables
-        }
-
+    interface Reference {
+      name: string
+      version: string
+      repository?: string
+      alias?: string
+      values?: Object
     }
 
-    interface Dependency {
-        references?: dependency.Reference[]
-        services?: Service[] // deployment.Service
-        proxies?: dependency.Proxy[]
-        variables?: dependency.Variables
+    interface Service {
+      group: string
+      name: string
+      version: string
+      port: number
+      ingress?: {
+        host: string
+        class: string
+        annotations?: object
+      }
     }
+
+    interface Proxy {
+      name: string
+      target: string
+    }
+
+    interface Variable {
+      name: string
+      value: string
+    }
+
+    interface Variables {
+      [key: string]: Variable[]
+    }
+
+    interface Declaration {
+      references?: Reference[]
+      services?: Service[] // dependency.Service
+      proxies?: Proxy[]
+      variables?: Variables
+    }
+
+  }
+
+  interface Dependency {
+    references?: dependency.Reference[]
+    services?: Service[] // deployment.Service
+    proxies?: dependency.Proxy[]
+    variables?: dependency.Variables
+  }
 
 }
 
 export namespace dependency {
-    export type Reference = toa.operations.deployment.dependency.Reference
-    export type Service = toa.operations.deployment.dependency.Service
-    export type Proxy = toa.operations.deployment.dependency.Proxy
-    export type Variables = toa.operations.deployment.dependency.Variables
+  export type Reference = toa.operations.deployment.dependency.Reference
+  export type Service = toa.operations.deployment.dependency.Service
+  export type Proxy = toa.operations.deployment.dependency.Proxy
+  export type Variables = toa.operations.deployment.dependency.Variables
 }
 
 export type Dependency = toa.operations.deployment.Dependency
