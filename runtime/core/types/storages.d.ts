70d567a15d6db977f19c627c6d6a5dbc700f48ae
temich <tema.gurtovoy@gmail.com>
2022-07-17 10:22:54 +0300

    docs(runtime): update migration.table type

--- runtime/core/types/storages.d.ts
+++ runtime/core/types/storages.d.ts
@@ -56,7 +56,7 @@ declare namespace toa.core.storages {
 
     database(name: string): Promise<void>
 
-    table(database: string, locator: Locator, schema: Object): Promise<void>
+    table(database: string, locator: Locator, schema: Object): Promise<string>
 
   }
 
