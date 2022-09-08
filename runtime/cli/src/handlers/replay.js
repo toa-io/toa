f7aec30aa25ec93af0f0dd4fff2143e4c134f236
temich <tema.gurtovoy@gmail.com>
2022-09-08 22:38:29 +0400

    feat(cli): add colors to `replay` output

--- runtime/cli/src/handlers/replay.js
+++ runtime/cli/src/handlers/replay.js
@@ -6,7 +6,16 @@ const find = require('../util/find')
 async function replay (argv) {
   const path = find.component(argv.path)
 
-  await component(path)
+  const ok = await component(path)
+  const message = ok ? GREEN + 'PASSED' : RED + 'FAILED'
+
+  process.on('beforeExit', () => {
+    console.log(message + RESET)
+  })
 }
+
+const GREEN = '\x1b[32m'
+const RED = '\x1b[31m'
+const RESET = '\x1b[0m'
 
 exports.replay = replay
