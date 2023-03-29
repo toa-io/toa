ae66d39ece9f8cc786d30f80ff465e4d8e3fa653
temich <tema.gurtovoy@gmail.com>
2023-03-29 11:06:47 +0400

    style(node): add blank lines

--- connectors/bridges.node/src/define/receivers.js
+++ connectors/bridges.node/src/define/receivers.js
@@ -4,11 +4,13 @@ const load = require('../load')
 
 const receiver = async (root, label) => {
   const module = await load.receiver(root, label)
+
   return definition(module)
 }
 
 const receivers = async (root) => {
   const modules = await load.receivers(root)
+
   return Object.fromEntries(modules.map(([name, module]) => [name, definition(module)]))
 }
 
