eb17d71a2a67b34d9eaab3cf279c43f0e98d746a
Илья Бурлак <Burlak ilia>
2023-04-08 18:33:47 +0300

    feat: add env cli logic

--- runtime/cli/src/handlers/env.js
+++ runtime/cli/src/handlers/env.js
@@ -1,6 +1,17 @@
 'use strict'
 
+const boot = require('@toa.io/boot')
+const { context: find } = require('../util/find')
+const { file } = require('@toa.io/filesystem')
+
 async function env (argv) {
+  const path = find(argv.path)
+  const operator = await boot.deployment(path, argv.environment)
+  const variables = operator.variables()
+
+  const envData = variables.global.reverse().map(item => `${item.name}=${item.value ?? ''}`)
+
+  await file.write(`${path}/.env`, envData.join('\n'))
 }
 
 exports.env = env
