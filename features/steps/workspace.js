508c948d6ac03a4c21be0dbceb05c7a53aae52f6
temich <tema.gurtovoy@gmail.com>
2023-04-08 13:36:36 +0400

    feat(features): add `I have environment` step

--- features/steps/workspace.js
+++ features/steps/workspace.js
@@ -1,10 +1,14 @@
 'use strict'
 
+const assert = require('node:assert')
+const { join } = require('node:path')
+const { subtract } = require('@toa.io/generic')
+const { file } = require('@toa.io/filesystem')
 const components = require('./.workspace/components')
 const samples = require('./.workspace/samples')
 const context = require('./.workspace/context')
 
-const { Given } = require('@cucumber/cucumber')
+const { Given, Then } = require('@cucumber/cucumber')
 
 Given('I have a component {component}',
   async function (component) {
@@ -45,3 +49,20 @@ Given('I have integration samples',
   async function () {
     await samples.copy(this.cwd)
   })
+
+Then('I have an environment with:',
+  /**
+   * @param {string} [search]
+   * @this {toa.features.Context}
+   */
+  async function (search) {
+    const searchLines = search.split('\n')
+    const path = join(this.cwd, ENV_FILE)
+    const contents = await file.read(path)
+    const existingLines = contents.split('\n')
+    const diff = subtract(searchLines, existingLines)
+
+    assert.equal(diff.length, 0, 'Environment does not contain at least one of the given lines')
+  })
+
+const ENV_FILE = '.env'
