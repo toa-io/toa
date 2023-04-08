7adde14185064c8b5aea2105a6faa7cd85f74374
Илья Бурлак <Burlak ilia>
2023-04-08 12:56:07 +0300

    feat: remove incorrect method

--- operations/types/deployment/operator.d.ts
+++ operations/types/deployment/operator.d.ts
@@ -14,8 +14,6 @@ declare namespace toa.deployment {
         install(options?: installation.Options): Promise<void>
 
         template(options?: template.Options): Promise<string>
-
-        env(): string
     }
 
 }
