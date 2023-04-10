e850a2bde494042c0f07e11d2b3cc4f797bf444c
temich <tema.gurtovoy@gmail.com>
2023-04-10 11:07:33 +0400

    fix(filesystem): refactor `file.exists` to `file.is`

--- libraries/filesystem/source/file/index.js
+++ libraries/filesystem/source/file/index.js
@@ -4,10 +4,10 @@ const { glob } = require('./glob')
 const { lines } = require('./lines')
 const { read } = require('./read')
 const { write } = require('./write')
-const { exists } = require('./exists')
+const { is } = require('./is')
 
 exports.glob = glob
 exports.lines = lines
 exports.read = read
 exports.write = write
-exports.exists = exists
+exports.is = is
