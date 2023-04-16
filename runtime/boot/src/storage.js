6a8ce6037562170c74129bcc74fc8ce7eb5c1b3f
temich <tema.gurtovoy@gmail.com>
2023-04-17 01:15:04 +0400

    fix(boot): fix custom storage with relative reference

--- runtime/boot/src/storage.js
+++ runtime/boot/src/storage.js
@@ -1,21 +1,41 @@
 'use strict'
 
+const { join } = require('node:path')
 const extensions = require('./extensions')
 
 /**
- * @param {toa.core.Locator} locator
- * @param {string} provider
- * @param {any} properties
+ * @param {toa.norm.Component} component
  * @returns {toa.core.Storage}
  */
-const storage = (locator, provider, properties) => {
-  const { Factory } = require(provider)
+const storage = (component) => {
+  const [Factory, properties] = load(component)
 
   /** @type {toa.core.storages.Factory} */
   const factory = new Factory()
-  const storage = factory.storage(locator, properties)
+  const storage = factory.storage(component.locator, properties)
 
   return extensions.storage(storage)
+}
+
+function load (component) {
+  const reference = component.entity.storage
+  const path = require.resolve(reference, { paths: [component.path] })
+  const { Factory } = require(path)
+
+  const pkg = loadPackageJson(component.path, reference)
+  const properties = pkg === null ? null : component.properties?.[pkg.name]
+
+  return [Factory, properties]
+}
+
+function loadPackageJson (root, reference) {
+  const path = join(root, reference, 'package.json')
+
+  try {
+    return require(path)
+  } catch {
+    return null
+  }
 }
 
 exports.storage = storage
