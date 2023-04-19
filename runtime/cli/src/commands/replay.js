4f767a7fce0588a69a880c53eeda1d09ad416e33
temich <tema.gurtovoy@gmail.com>
2023-04-19 23:23:30 +0400

    docs(cli): add `--component` flag to `toa replay`

--- runtime/cli/src/commands/replay.js
+++ runtime/cli/src/commands/replay.js
@@ -11,6 +11,30 @@ const builder = (yargs) => {
       desc: 'Paths to components or context',
       default: '.'
     })
+    .option('component', {
+      alias: 'c',
+      type: 'string',
+      group: 'Command options:',
+      describe: 'Replay samples for specified component'
+    })
+    .option('integration', {
+      alias: 'i',
+      type: 'boolean',
+      group: 'Command options:',
+      describe: 'Replay integration tests only'
+    })
+    .option('sample', {
+      alias: 's',
+      type: 'string',
+      group: 'Command options:',
+      describe: 'Replay samples found by glob pattern'
+    })
+    .option('title', {
+      alias: 't',
+      type: 'string',
+      group: 'Command options:',
+      describe: 'Replay samples with titles matching regexp'
+    })
 }
 
 exports.command = 'replay [paths...]'
