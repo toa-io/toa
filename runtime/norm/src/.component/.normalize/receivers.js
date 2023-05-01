315d438eb261fa6ea3acfe13ec2cfebb9cf2d9a1
Burlak Ilia <i.l.burlak@gmail.com>
2023-05-01 11:41:18 +0400

    fix(norm): do not add default prefix for non default source

--- runtime/norm/src/.component/.normalize/receivers.js
+++ runtime/norm/src/.component/.normalize/receivers.js
@@ -7,7 +7,9 @@ function receivers (component) {
 
   for (const [key, value] of Object.entries(receivers)) {
     const segments = key.split('.')
+    const source = value.source ?? 'default'
 
+    if (source !== 'default') continue
     if (segments.length === 3) continue
 
     const newKey = 'default.' + key
