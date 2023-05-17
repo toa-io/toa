40e91b7ab828ed80b656923de25b33e181f79d72
temich <tema.gurtovoy@gmail.com>
2023-05-17 10:13:01 +0400

    feat(cli): add `environment` to `export images`

--- runtime/cli/src/handlers/export/images.js
+++ runtime/cli/src/handlers/export/images.js
@@ -7,7 +7,7 @@ const { context: find } = require('../../util/find')
 
 const prepare = async (argv) => {
   const context = find(argv.path)
-  const operator = await boot.deployment(context)
+  const operator = await boot.deployment(context, argv.environment)
   const path = await operator.prepare(argv.target)
 
   console.log(path)
