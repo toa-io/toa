7905453c7c41a1021c3df5aaaf6059757a85262a
temich <tema.gurtovoy@gmail.com>
2023-06-16 00:23:55 +0400

    chore: add `cucumber-tsflow`

---
+++ extensions/exposition/features/test.feature
@@ -0,0 +1,6 @@
+Feature: Example Feature
+
+  Scenario: Adding two numbers
+    Given I enter '2' and '8'
+    And Ok test
+    Then I receive the result '10'
--- extensions/exposition/features/test.ts
+++ extensions/exposition/features/test.ts
@@ -1,5 +1,22 @@
-import { Given } from '@cucumber/cucumber'
+import { binding, given, then } from 'cucumber-tsflow'
 
-Given('scenario is working', function () {
-  console.log('ok')
-})
+@binding()
+export class ArithmeticSteps {
+  private computedResult: number = 0
+
+  @given(/I enter '(\d*)' and '(\d*)'/)
+  public givenTwoNumbers (num1: string, num2: string): void {
+    this.computedResult = parseInt(num1) + parseInt(num2)
+  }
+
+  @then(/I receive the result '(\d*)'/)
+  public thenResultReceived (expectedResult: string): void {
+    if (parseInt(expectedResult) !== this.computedResult)
+      throw new Error('Arithmetic Error')
+  }
+
+  @given('Ok {word}')
+  public okay (word: string): void {
+
+  }
+}
