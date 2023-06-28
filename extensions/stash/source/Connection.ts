265f52514a68349c2acb9c3bf6423367251e901f
temich <tema.gurtovoy@gmail.com>
2023-06-28 13:43:29 +0400

    feat(stash): add logging

--- extensions/stash/source/Connection.ts
+++ extensions/stash/source/Connection.ts
@@ -3,6 +3,7 @@ import { Connector, type Locator } from '@toa.io/core'
 
 export class Connection extends Connector {
   public readonly redis: Redis
+  private readonly url: string
 
   public constructor (url: string, locator: Locator) {
     super()
@@ -11,13 +12,18 @@ export class Connection extends Connector {
     const options: ClusterOptions = { keyPrefix, enableReadyCheck: true, lazyConnect: true }
 
     this.redis = new Redis(url, options)
+    this.url = url
   }
 
   protected override async open (): Promise<void> {
     await this.redis.connect()
+
+    console.log('Stash connected to ' + this.url)
   }
 
   protected override async close (): Promise<void> {
     this.redis.disconnect()
+
+    console.log('Stash disconnected from ' + this.url)
   }
 }
