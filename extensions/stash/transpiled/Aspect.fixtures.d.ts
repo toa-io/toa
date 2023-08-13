dc1be569d65eddbb1edd7d73a9f42da7cd6e1cd4
temich <tema.gurtovoy@gmail.com>
2023-08-13 15:05:26 +0400

    test(stash): remove tests

--- extensions/stash/transpiled/Aspect.fixtures.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="jest" />
-export declare const Redlock: jest.Mock<{
-    using: jest.Mock<Promise<undefined>, []>;
-}, []>;
--- extensions/stash/transpiled/Aspect.fixtures.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Redlock = void 0;
-exports.Redlock = jest.fn(() => ({
-    using: jest.fn(async () => undefined)
-}));
-//# sourceMappingURL=Aspect.fixtures.js.map
