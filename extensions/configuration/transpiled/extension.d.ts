7857bf04133532d6e616b7207766a92d4a7b4de1
temich <tema.gurtovoy@gmail.com>
2023-08-14 12:27:43 +0400

    chore: add transpile script

--- extensions/configuration/transpiled/extension.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Dependency } from '@toa.io/operations';
-import type { context } from '@toa.io/norm';
-export declare function deployment(instances: Instance[], annotation: any): Dependency;
-interface Manifest {
-    schema: string;
-}
-type Instance = context.Dependency<Manifest>;
-export {};
