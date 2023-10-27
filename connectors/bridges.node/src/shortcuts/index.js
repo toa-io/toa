ff02e3b6807381a76a317ae914c6ab45ecf250de
temich <tema.gurtovoy@gmail.com>
2023-10-28 02:39:05 +0400

    feat(node): add `storages` shortcut

--- connectors/bridges.node/src/shortcuts/index.js
+++ connectors/bridges.node/src/shortcuts/index.js
@@ -5,9 +5,11 @@ const { amqp } = require('./amqp')
 const { configuration } = require('./configuration')
 const { state } = require('./state')
 const { stash } = require('./stash')
+const { storages } = require('./storages')
 
 exports.http = http
 exports.amqp = amqp
 exports.configuration = configuration
 exports.state = state
 exports.stash = stash
+exports.storages = storages
