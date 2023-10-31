d1194ce8abc018bf24f773500e2851d47f40e5d2
temich <tema.gurtovoy@gmail.com>
2023-10-31 13:13:27 +0400

    feat(http): expose `parse.response`

--- libraries/http/source/request.ts
+++ libraries/http/source/request.ts
@@ -5,18 +5,6 @@ export async function request (http: string, origin?: string): Promise<string> {
 
   const reference = new URL(url, origin).href
   const response = await fetch(reference, { method, headers, body })
-  const statusLine = `${response.status} ${response.statusText}\n`
-  const headerLines = stringifyHeaders(response.headers) + '\n'
-  const responseText = await response.text()
 
-  return statusLine + headerLines + responseText
-}
-
-function stringifyHeaders (headers: Headers): string {
-  let lines = ''
-
-  headers.forEach((value, name) =>
-    (lines += `${name}: ${value}\n`))
-
-  return lines
+  return await parse.response(response)
 }
