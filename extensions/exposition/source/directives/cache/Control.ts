c333fed1bbbe71066d2e0af7635653ac341d2029
aircrisp <aircrisp@yandex.ru>
2023-11-03 02:50:52 +0400

    feat(exposition): extend cache:control with implicit logic

--- extensions/exposition/source/directives/cache/Control.ts
+++ extensions/exposition/source/directives/cache/Control.ts
@@ -1,19 +1,35 @@
-import { type Input } from '../../Directive'
-import { type Directive } from './types'
+import { parse } from '@tusbar/cache-control'
+import { type PostProcessInput, type Directive } from './types'
 import { isSafeMethod } from './utils'
 
 export class Control implements Directive {
   private readonly value: string
+  private readonly isPublic: boolean
+  private readonly isPrivate: boolean
+  private readonly isNoCache: boolean
 
   public constructor (value: string) {
-    this.value = value
+    const parsedValue = parse(value)
+
+    this.value = parsedValue.format()
+
+    this.isPublic = parsedValue.public === true
+    this.isPrivate = parsedValue.private === true
+    this.isNoCache = parsedValue.noCache === true
   }
 
-  public postProcess (request: Input): Headers {
+  public postProcess (request: PostProcessInput): Headers {
     const additionalHeaders: Headers = new Headers()
 
-    if (isSafeMethod(request.method))
-      additionalHeaders.set('cache-control', this.value)
+    if (!isSafeMethod(request.method)) return additionalHeaders
+
+    additionalHeaders.set('cache-control', this.value)
+
+    if (request.identity !== null && this.isPublic && !this.isNoCache)
+      additionalHeaders.append('cache-control', 'no-cache')
+
+    if (request.identity !== null && !this.isPublic && !this.isPrivate)
+      additionalHeaders.append('cache-control', 'private')
 
     return additionalHeaders
   }
