70b12bae6614f2c227463418a296af9941bde524
Konstantin Vyatkin <tino@vtkn.io>
2024-01-26 12:32:39 +0100

    fix: dereference schema at toa export manifest

--- runtime/cli/src/handlers/export/manifest.js
+++ runtime/cli/src/handlers/export/manifest.js
@@ -3,6 +3,7 @@
 const { component } = require('@toa.io/norm')
 const { console } = require('@toa.io/console')
 const yaml = require('@toa.io/yaml')
+const { definitions: loadDefinitions } = require('@toa.io/schema/source/definitions')
 
 const { components: find } = require('../../util/find')
 
@@ -14,10 +15,17 @@ const print = async (argv) => {
   const manifest = await component(path)
 
   if (argv.error !== true) {
+    // FIXME: workaround for unresolvable definitions references in the schema
+    const defs = []
+    loadDefinitions({
+      addSchema: defs.push.bind(defs),
+    })
+    const { definitions, $id } = defs[0]
+    const dereferenced = JSON.parse(JSON.stringify(manifest).replaceAll($id, ''))
+    dereferenced.entity.schema.definitions = definitions
     const result = argv.output === 'json'
-      ? JSON.stringify(manifest, null, 2)
-      : yaml.dump(manifest)
-
+      ? JSON.stringify(dereferenced, null, 2)
+      : yaml.dump(dereferenced)
     console.log(result)
   }
 }
