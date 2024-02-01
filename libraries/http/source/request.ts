206620be7fb04f121f0091a33c4346f3af4eb3f4
temich <tema.gurtovoy@gmail.com>
2024-02-01 17:29:31 +0400

    feat(http): return response from `Agent.request`

--- libraries/http/source/request.ts
+++ libraries/http/source/request.ts
@@ -1,10 +1,9 @@
 import * as parse from './parse'
 
-export async function request (http: string, origin?: string): Promise<string> {
+export async function request (http: string, origin?: string): Promise<Response> {
   const { method, url, headers, body } = parse.request(http)
 
   const reference = new URL(url, origin).href
-  const response = await fetch(reference, { method, headers, body })
 
-  return await parse.response(response)
+  return await fetch(reference, { method, headers, body })
 }
