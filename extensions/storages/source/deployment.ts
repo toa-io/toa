b092d20f0acc0038671b5ad6550c167ca7a7f665
Konstantin Vyatkin <tino@vtkn.io>
2024-02-08 22:54:02 +0100

    feat: storage provides settings as objects

--- extensions/storages/source/deployment.ts
+++ extensions/storages/source/deployment.ts
@@ -1,13 +1,16 @@
+import * as assert from 'node:assert'
 import { encode } from 'msgpackr'
 import { providers } from './providers'
 import type { Dependency, Variable } from '@toa.io/operations'
 import type { context } from '@toa.io/norm'
 
+export const SERIALIZATION_PREFIX = 'TOA_STORAGES'
+
 export function deployment (instances: Instance[], annotation: Annotation): Dependency {
   validate(instances, annotation)
 
   const value = encode(annotation).toString('base64')
-  const pointer: Variable = { name: 'TOA_STORAGES', value }
+  const pointer: Variable = { name: SERIALIZATION_PREFIX, value }
   const secrets = getSecrets(annotation)
 
   return {
@@ -16,50 +19,53 @@ export function deployment (instances: Instance[], annotation: Annotation): Depe
 }
 
 function validate (instances: Instance[], annotation: Annotation): void {
-  if (annotation === undefined)
-    throw new Error('Storages annotation is required by: ' +
-      `'${instances.map((i) => i.component.locator.id).join("', '")}'`)
+  assert.ok(annotation !== undefined,
+    `Storages annotation is required by: '${instances
+      .map((i) => i.component.locator.id)
+      .join("', '")}'`)
 
-  for (const instance of instances)
-    contains(instance, annotation)
+  for (const instance of instances) contains(instance, annotation)
 
-  for (const ref of Object.values(annotation)) {
-    const url = new URL(ref)
-
-    if (!(url.protocol in providers))
-      throw new Error(`Unknown storage provider '${url.protocol}'`)
-  }
+  for (const { provider } of Object.values(annotation))
+    validateProviderId(provider)
 }
 
 function contains (instance: Instance, annotation: Annotation): void {
   for (const name of instance.manifest)
-    if (!(name in annotation))
-      throw new Error(`Missing '${name}' storage annotation ` +
+    assert.ok(name in annotation,
+      `Missing '${name}' storage annotation ` +
         `declared in '${instance.component.locator.id}'`)
 }
 
-function getSecrets (annotation: Annotation): Variable[] {
+export function validateProviderId (id: string | undefined): asserts id is keyof typeof providers {
+  assert.ok(typeof id === 'string' && id in providers, `Unknown storage provider '${id}'`)
+}
+
+function getSecrets (annotation: Readonly<Annotation>): Variable[] {
   const secrets: Variable[] = []
 
-  for (const [storage, ref] of Object.entries(annotation)) {
-    const url = new URL(ref)
-    const Provider = providers[url.protocol]
+  for (const [storage, props] of Object.entries(annotation)) {
+    const providerId = props.provider
 
-    if (Provider.SECRETS === undefined)
-      continue
+    validateProviderId(providerId)
+
+    const Provider = providers[providerId]
+
+    if (Provider.SECRETS.length === 0) continue
 
     for (const secret of Provider.SECRETS)
       secrets.push({
-        name: `TOA_STORAGES_${storage.toUpperCase()}_${secret.toUpperCase()}`,
+        name: `${SERIALIZATION_PREFIX}_${storage.toUpperCase()}_${secret.name.toUpperCase()}`,
         secret: {
           name: `toa-storages-${storage}`,
-          key: secret
+          key: secret.name,
+          optional: secret.optional
         }
-      } satisfies Variable)
+      })
   }
 
   return secrets
 }
 
-type Annotation = Record<string, string>
+type Annotation = Record<string, { [k: string]: unknown, provider: string }>
 export type Instance = context.Dependency<string[]>
--- extensions/storages/source/providers/index.test.ts
+++ extensions/storages/source/providers/index.test.ts
@@ -1,21 +1,26 @@
 import { type Readable } from 'node:stream'
-import { buffer } from '@toa.io/streams'
-import { cases, open, rnd, read } from '../test/util'
+import { buffer } from 'node:stream/consumers'
+import { readFile } from 'node:fs/promises'
+import { createReadStream } from 'node:fs'
+import path from 'node:path'
+import { randomUUID } from 'node:crypto'
+import { suites } from '../test/util'
 import { providers } from './index'
 
-describe.each(cases)('%s', (...suite) => {
-  const [protocol, url, secrets, init] = suite
-  const Provider = providers[protocol]
-  const provider = new Provider(url, secrets)
+describe.each(suites)('$provider', ({ provider: providerId, run, ...props }) => {
+  const it = run ? global.it : global.it.skip
+
+  const Provider = providers[providerId]
+  const provider = new Provider(props as any)
 
   let dir: string
 
   beforeAll(async () => {
-    await init?.(url, secrets)
+    process.chdir(path.resolve(__dirname, '../test/'))
   })
 
   beforeEach(() => {
-    dir = '/' + rnd()
+    dir = '/' + randomUUID()
   })
 
   it('should be', async () => {
@@ -23,39 +28,39 @@ describe.each(cases)('%s', (...suite) => {
   })
 
   it('should return null if file not found', async () => {
-    const result = await provider.get(rnd())
+    const result = await provider.get(randomUUID())
 
     expect(result).toBeNull()
   })
 
   it('should create entry', async () => {
-    const stream = open('lenna.png')
+    const stream = createReadStream('lenna.png')
 
     await provider.put(dir, 'lenna.png', stream)
 
     const readable = await provider.get(dir + '/lenna.png') as Readable
     const output = await buffer(readable)
-    const lenna = await read('lenna.png')
+    const lenna = await readFile('lenna.png')
 
     expect(output.compare(lenna)).toBe(0)
   })
 
   it('should overwrite existing entry', async () => {
-    const stream0 = open('lenna.png')
-    const stream1 = open('albert.jpg')
+    const stream0 = createReadStream('lenna.png')
+    const stream1 = createReadStream('albert.jpg')
 
     await provider.put(dir, 'lenna.png', stream0)
     await provider.put(dir, 'lenna.png', stream1)
 
     const readable = await provider.get(dir + '/lenna.png') as Readable
     const output = await buffer(readable)
-    const albert = await read('albert.jpg')
+    const albert = await readFile('albert.jpg')
 
     expect(output.compare(albert)).toBe(0)
   })
 
   it('should get by path', async () => {
-    const stream = open('lenna.png')
+    const stream = createReadStream('lenna.png')
 
     await provider.put(dir, 'lenna.png', stream)
 
@@ -77,7 +82,7 @@ describe.each(cases)('%s', (...suite) => {
      */
 
     it('should delete entry', async () => {
-      const stream = open('lenna.png')
+      const stream = createReadStream('lenna.png')
 
       await provider.put(dir, 'lenna.png', stream)
       await provider.delete(dir + '/lenna.png')
@@ -92,7 +97,7 @@ describe.each(cases)('%s', (...suite) => {
     })
 
     it('should delete directory', async () => {
-      const stream = open('lenna.png')
+      const stream = createReadStream('lenna.png')
 
       await provider.put(dir, 'lenna.png', stream)
       await provider.delete(dir)
@@ -103,8 +108,8 @@ describe.each(cases)('%s', (...suite) => {
     })
 
     it('should move an entry', async () => {
-      const stream = open('lenna.png')
-      const dir2 = '/' + rnd()
+      const stream = createReadStream('lenna.png')
+      const dir2 = '/' + randomUUID()
 
       await provider.put(dir, 'lenna.png', stream)
       await provider.move(dir + '/lenna.png', dir2 + '/lenna2.png')
