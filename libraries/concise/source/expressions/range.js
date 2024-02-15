f0e4a9adc6c89309375399b972e806d1fa92600e
temich <tema.gurtovoy@gmail.com>
2024-02-15 20:15:05 +0400

    feat(concise): add numeric ranges

---
+++ libraries/concise/source/expressions/range.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/**
+ * @param {string} value
+ * @returns {object | null}
+ */
+function range (value) {
+  if (typeof value !== 'string')
+    return null
+
+  const match = value.match(RX)
+
+  if (match === null)
+    return null
+
+  const schema = { type: match.groups.type }
+  const parse = PARSERS[schema.type]
+
+  if (match.groups.min !== '') {
+    const keyword = value[schema.type.length] === '[' ? 'minimum' : 'exclusiveMinimum'
+
+    schema[keyword] = parse(match.groups.min)
+  }
+
+  if (match.groups.max !== '') {
+    const keyword = value[value.length - 1] === ']' ? 'maximum' : 'exclusiveMaximum'
+
+    schema[keyword] = parse(match.groups.max)
+  }
+
+  return schema
+}
+
+const PARSERS = {
+  'integer': parseInt,
+  'number': parseFloat
+}
+
+const RX = /^(?<type>integer|number)[[(]\s?(?<min>[\d.]{0,32})\s?,\s?(?<max>[\d.]{0,32})\s?[)\]]$/
+
+exports.range = range
