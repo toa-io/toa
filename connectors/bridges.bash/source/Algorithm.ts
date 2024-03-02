6cca7d2336c39d3b6d0466b0dc1e660e84e758a0
temich <tema.gurtovoy@gmail.com>
2024-03-02 15:04:34 +0400

    refactor(bash): replace `process.env.SHELL` with `os.userInfo()`

--- connectors/bridges.bash/source/Algorithm.ts
+++ connectors/bridges.bash/source/Algorithm.ts
@@ -1,40 +1,45 @@
-import util from 'node:util'
-import child from 'node:child_process'
+import { spawnSync } from 'node:child_process'
+import { userInfo } from 'node:os'
 import { Connector } from '@toa.io/core'
 import type { bridges, Reply } from '@toa.io/core'
 
-const exec = util.promisify(child.exec)
-
 export class Algorithm extends Connector implements bridges.Algorithm {
+  private readonly shell: string
   private readonly path: string
 
   public constructor (path: string) {
     super()
+
+    const shell = userInfo().shell
+
+    if (shell === null)
+      throw new Error('The shell is not available. Am I running on Windows?')
+
+    this.shell = shell
     this.path = path
   }
 
   public async mount (): Promise<void> {
-
   }
 
   public async execute (input: Record<string, unknown> | undefined | null): Promise<Reply> {
     const args = (input === undefined || input === null)
-      ? ''
-      : Object.entries(input).map(([key, value]) => `--${key} ${value?.toString()}`).join(' ')
+      ? []
+      : Object.entries(input).map(([key, value]) => ['--' + key, value?.toString() ?? '']).flat()
 
-    const command = process.env.SHELL + ` ${this.path}${args === '' ? '' : ` ${args}`}`
+    const result = spawnSync(this.shell, [this.path].concat(args), { shell: true })
 
-    try {
-      const result = await exec(command)
+    if (result.status === 0) {
+      const output = result.stdout.toString().trim()
 
-      return { output: result.stdout.trim() }
-    } catch (error: any) {
-      if (error.code !== 1)
+      return { output }
+    } else {
+      const error = new Error(result.stderr.toString().trim())
+
+      if (result.status === 1)
+        return { output: error }
+      else
         throw error
-
-      const message: string | undefined = error.stderr?.trim()
-
-      return { output: new Error(message) }
     }
   }
 }
