853fba5789be07c8ae24669ad2abf399787b2d28
temich <tema.gurtovoy@gmail.com>
2024-03-05 21:26:07 +0400

    fix(exposition): fix role regular expression

--- extensions/exposition/source/exceptions.ts
+++ extensions/exposition/source/exceptions.ts
@@ -6,17 +6,15 @@ export function rethrow (exception: Exception): void {
   // see /runtime/core/src/exceptions.js
 
   throw match<Error>(exception.code,
-    badRequest(exception), () => new http.BadRequest(exception.message),
+    badRequest, () => new http.BadRequest(exception.message),
     302, NOT_FOUND,
     303, PRECONDITION_FAILED,
     306, () => new http.Conflict(exception.message),
     () => exception)
 }
 
-function badRequest (exception: Exception) {
-  return (code: number): boolean => {
-    return (exception.code >= 200 && exception.code < 210) || exception.code === 221
-  }
+function badRequest (code: number): boolean {
+  return (code >= 200 && code < 210) || code === 221
 }
 
 const NOT_FOUND = new http.NotFound()
