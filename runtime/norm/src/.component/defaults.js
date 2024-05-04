6a28ec4ae60e620cc57d50e325cce090620eb06a
temich <tema.gurtovoy@gmail.com>
2024-05-04 10:02:24 +0400

    feat(norm): add default component naming

--- runtime/norm/src/.component/defaults.js
+++ runtime/norm/src/.component/defaults.js
@@ -4,8 +4,11 @@ const { hash } = require('@toa.io/generic')
 const assert = require('node:assert')
 
 // these defaults are required before validation
-const defaults = (manifest) => {
-  if (manifest.name === undefined) manifest.name = protoName(manifest)
+const defaults = (manifest, proto) => {
+  if (manifest.name === undefined)
+    if (proto) manifest.name = protoName(manifest)
+    else nameAfterDir(manifest)
+
   if (manifest.bindings === undefined) manifest.bindings = ['@toa.io/bindings.amqp']
   if (manifest.bridge === undefined) manifest.bridge = '@toa.io/bridges.node'
 
@@ -22,6 +25,15 @@ const defaults = (manifest) => {
 
 function protoName (manifest) {
   return 'proto' + hash(manifest.path)
+}
+
+function nameAfterDir (manifest) {
+  const parts = manifest.path.split('/')
+  const dirname = parts[parts.length - 1]
+  const [name, namespace] = dirname.split('.').reverse()
+
+  manifest.name = name
+  manifest.namespace = namespace
 }
 
 exports.defaults = defaults
