3ac423c3fecf21e30425b3d91b2464e1ab94b22b
temich <tema.gurtovoy@gmail.com>
2024-06-23 17:12:17 +0400

    feat(agent): add `length` parameter to `id` function

--- libraries/agent/source/functions/id.ts
+++ libraries/agent/source/functions/id.ts
@@ -1,5 +1,5 @@
 import { newid } from '@toa.io/generic'
 
-export function id (): string {
-  return newid()
+export function id (_: unknown, length?: string): string {
+  return newid().slice(0, length === undefined ? 32 : Number.parseInt(length))
 }
