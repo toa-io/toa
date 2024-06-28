78b8bb6d019fc146db2f91d5ec76e81a406cf545
temich <tema.gurtovoy@gmail.com>
2024-06-29 00:19:07 +0400

    fix(configuration): fix non-string secrets deployment

--- extensions/configuration/source/manifest.ts
+++ extensions/configuration/source/manifest.ts
@@ -2,7 +2,8 @@ import * as schemas from './schemas'
 import { type Node } from './configuration'
 
 export function manifest (manifest: Manifest): Manifest {
-  if (manifest.schema === undefined) manifest = { schema: manifest }
+  if (manifest.schema === undefined)
+    manifest = { schema: manifest }
 
   schemas.manifest.validate(manifest)
 
