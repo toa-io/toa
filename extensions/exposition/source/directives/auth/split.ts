bf98be1d44e9dbf83111db9ce0c87680904a97ed
temich <tema.gurtovoy@gmail.com>
2024-07-03 11:56:35 +0400

    test(exposition): fix expected error messages

--- extensions/exposition/source/directives/auth/split.ts
+++ extensions/exposition/source/directives/auth/split.ts
@@ -5,7 +5,7 @@ export function split (authorization: string): [Scheme, string] {
   const space = authorization.indexOf(' ')
 
   if (space === -1)
-    throw new http.Unauthorized('Malformed authorization header.')
+    throw new http.Unauthorized('Malformed authorization header')
 
   const Scheme = authorization.slice(0, space)
   const scheme = Scheme.toLowerCase() as Scheme
