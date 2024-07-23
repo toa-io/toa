ac668a3157ae979140e8cc46b44fedf7e51f918e
temich <tema.gurtovoy@gmail.com>
2024-07-23 09:41:45 +0400

    refactor(telemetry): shorten operation logs context

--- extensions/telemetry/source/Logs.ts
+++ extensions/telemetry/source/Logs.ts
@@ -5,20 +5,24 @@ import type { Locator, extensions } from '@toa.io/core'
 
 export class Logs extends Connector implements extensions.Aspect {
   public readonly name = 'logs'
+  private readonly locator: Locator
   private readonly console: Console
   private readonly consoles: Record<string, Console> = {}
 
-  public constructor ({ namespace, name: component }: Locator, options: LogsOptions) {
+  public constructor (locator: Locator, options: LogsOptions) {
     super()
 
-    this.console = console.fork({ namespace, component })
+    this.locator = locator
+    this.console = console.fork()
     this.console.configure(options)
   }
 
   // eslint-disable-next-line max-params
   public invoke (operation: string, severity: Channel, message: string, attributes?: object): void {
     if (!(operation in this.consoles))
-      this.consoles[operation] = this.console.fork({ operation })
+      this.consoles[operation] = this.console.fork({
+        endpoint: `${this.locator.id}.${operation}`
+      })
 
     this.consoles[operation][severity](message, attributes)
   }
