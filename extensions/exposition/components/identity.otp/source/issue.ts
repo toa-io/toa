1a445a7ee5d4af0980268151ee1b229bf7821939
temich <tema.gurtovoy@gmail.com>
2025-12-19 18:01:28 +0400

    feat(exposition): add OTP lifetime input parameter

--- extensions/exposition/components/identity.otp/source/issue.ts
+++ extensions/exposition/components/identity.otp/source/issue.ts
@@ -4,17 +4,19 @@ export async function effect (input: Input, context: Context): Promise<Output> {
   const { authority, username } = input
   const code = Math.floor(100000 + Math.random() * 900000).toString()
   const key = `${authority}:${username}:${code}`
+  const lifetime = input.lifetime ?? context.configuration.lifetime
 
-  context.logs.debug('Issue OTP', { authority, username, code })
+  context.logs.debug('Issue OTP', { authority, username, code, lifetime })
 
-  await context.stash.set(key, 1, 'EX', context.configuration.lifetime)
-
+  await context.stash.set(key, 1, 'EX', lifetime)
+  
   return { code }
 }
 
 interface Input {
   authority: string
   username: string
+  lifetime?: number
 }
 
 interface Output {
